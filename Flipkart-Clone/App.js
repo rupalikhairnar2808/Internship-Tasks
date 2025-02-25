8beb049cab20dbb66ffb512b39c276bd93895207
rupalikhairnar2808 <130838260+rupalikhairnar2808@users.noreply.github.com>
2025-02-25 17:11:22 +0530

    Add files via upload

---
+++ Flipkart-Clone/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Welcome to the Flipkart Clone</h1>
+      {/* You can add other components here */}
+    </div>
+  );
+}
+
+export default App;
